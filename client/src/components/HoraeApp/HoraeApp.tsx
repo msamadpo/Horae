5a6dac5815cc3d5fd166381e9daded0c9d770048
Karthik Manickam <karthik.omanickam@gmail.com>
2020-05-07 17:59:18 -0700

    Created boilerplate calendar component

--- client/src/components/HoraeApp/HoraeApp.tsx
+++ client/src/components/HoraeApp/HoraeApp.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import AppNavbar from 'components/HoraeApp/AppNavbar';
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
-
 import styled from 'styled-components';
+
 import calendarIcon from 'assets/img/calendar-icon.svg';
 import todoIcon from 'assets/img/todo-icon.svg';
 import userIcon from 'assets/img/user-icon.svg';
+
 import Text from 'components/Common/Text';
+import AppNavbar from 'components/HoraeApp/AppNavbar';
+import Calendar from 'components/HoraeApp/Calendar';
 
 const ITEMS = [
   {
@@ -46,11 +48,7 @@ function HoraeApp() {
         <AppNavbar items={ITEMS} />
         <StyledAppBody>
           <Switch>
-            <Route path="/calendar" exact>
-              <Text weight="400" type="heading1">
-                Calendar
-              </Text>
-            </Route>
+            <Route path="/calendar" component={Calendar} exact />
             <Route path="/todo" exact>
               <Text weight="400" type="heading1">
                 Todo
