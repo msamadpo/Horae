6fe92b2b9b1448f367af65774e97dde2e7e3db3a
Karthik Manickam <karthik.omanickam@gmail.com>
2020-05-17 13:39:19 -0700

    Implemented reducer logic for removing tasks

--- client/src/components/HoraeApp/TodoPage/TodoPage.tsx
+++ client/src/components/HoraeApp/TodoPage/TodoPage.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GlobalContext from 'context/GlobalContext';
 
 function TodoPage() {
-  return <div>Todo Page</div>;
+  const { data, dispatch } = useContext(GlobalContext);
+  const tasks = data.todo_lists[0].tasks;
+
+  const removeTask = () => {
+    dispatch({
+      type: 'REMOVE_TASK',
+      payload: {
+        taskListId: '123456789',
+        taskId: '343ERHFGIR4545',
+      },
+    });
+  };
+  return (
+    <div onClick={removeTask}>
+      Todo Page
+      <div>
+        {tasks.map((task) => (
+          <div>{task.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TodoPage;
--- client/src/context/GlobalReducer.ts
+++ client/src/context/GlobalReducer.ts
@@ -25,12 +25,11 @@ export type CalendarEvent = {
 export type GlobalState = typeof mockData;
 
 export type Action =
+  | { type: 'ADD_TASK'; payload: { taskListId: string; task: TaskPayload } }
+  | { type: 'REMOVE_TASK'; payload: { taskListId: string; taskId: string } }
   | {
-      type: 'ADD_TASK' | 'REMOVE_TASK' | 'EDIT_TASK';
-      payload: {
-        taskListId: string;
-        task: TaskPayload;
-      };
+      type: 'EDIT_TASK';
+      payload: { taskListId: string; taskId: string; task: TaskPayload };
     }
   | {
       type:
@@ -45,7 +44,7 @@ export default function globalReducer(
   action: Action
 ): GlobalState {
   switch (action.type) {
-    case 'ADD_TASK':
+    case 'ADD_TASK': {
       const ID_HACK = Math.floor(Math.random() * 10000).toString(); // TODO Get the correct UUID from server
       const newTask = {
         id: ID_HACK,
@@ -63,9 +62,25 @@ export default function globalReducer(
       );
       console.log('Dispatch ADD_TASK');
       return { ...state, todo_lists: newTaskLists };
-    case 'REMOVE_TASK':
+    }
+    case 'REMOVE_TASK': {
+      const currentList = {
+        ...state.todo_lists.filter(
+          (list) => list.id === action.payload.taskListId
+        )[0],
+      };
+      const newTaskList = {
+        ...currentList,
+        tasks: currentList.tasks.filter(
+          (task) => task.id !== action.payload.taskId
+        ),
+      };
+      const newTaskLists = state.todo_lists.map((todoList) =>
+        todoList.id === action.payload.taskListId ? newTaskList : todoList
+      );
       console.log('Dispatch REMOVE_TASK');
-      return state;
+      return { ...state, todo_lists: newTaskLists };
+    }
     case 'EDIT_TASK':
       console.log('Dispatch EDIT_TASK');
       return state;
