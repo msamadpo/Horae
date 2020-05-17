7978987d60d35a91457385307ed822b3882ec3e9
Karthik Manickam <karthik.omanickam@gmail.com>
2020-05-17 13:55:01 -0700

    Simple hardcoded examples of adding, editing, removing tasks

--- client/src/components/HoraeApp/TodoPage/TodoPage.tsx
+++ client/src/components/HoraeApp/TodoPage/TodoPage.tsx
@@ -5,6 +5,32 @@ function TodoPage() {
   const { data, dispatch } = useContext(GlobalContext);
   const tasks = data.todo_lists[0].tasks;
 
+  const addTask = () => {
+    dispatch({
+      type: 'ADD_TASK',
+      payload: {
+        taskListId: '123456789',
+        task: {
+          name: 'New Task',
+          completed: false,
+        },
+      },
+    });
+  };
+
+  const editTask = () => {
+    dispatch({
+      type: 'EDIT_TASK',
+      payload: {
+        taskListId: '123456789',
+        taskId: '343ERHFGIR4545',
+        task: {
+          name: 'Edited Spinach',
+        },
+      },
+    });
+  };
+
   const removeTask = () => {
     dispatch({
       type: 'REMOVE_TASK',
@@ -14,13 +40,19 @@ function TodoPage() {
       },
     });
   };
+
   return (
-    <div onClick={removeTask}>
+    <div>
       Todo Page
       <div>
         {tasks.map((task) => (
           <div>{task.name}</div>
         ))}
+      </div>
+      <div>
+        <button onClick={addTask}>Add task</button>
+        <button onClick={removeTask}>Remove task</button>
+        <button onClick={editTask}>Edit task</button>
       </div>
     </div>
   );
